dd5515f44b60172604dfa4ac87083da1ce0c1acd
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-04-27 10:10:32 +0300

    Add files via upload

---
+++ UnitTest5.1C/UnitTest5.1C.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project5.1C/Money.cpp"
+#include "../Project5.1C/MyException.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51C
+{
+	TEST_CLASS(UnitTest51C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Money a(0,0,0,0,0,0,0,0,0,0,0);
+			bool k = a.getk500();
+			Assert::AreEqual(k, false);
+		}
+	};
+}
